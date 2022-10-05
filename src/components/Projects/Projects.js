81557c6b4f1bfe659bec575108222e7f8e1be1d4
Georgi <georgichichev15@Gmail.com>
2022-10-06 01:44:00 +0300

    Add projects

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,17 @@
+const Projects = () => {
+    return(
+        <section className="projects">
+            <h3>Projects (Design & Develop)</h3>
+            <article className="project">
+                <a href="https://chatterboxer-9c635.firebaseapp.com" target="_blank">Chatterbox</a>
+                <p>React, Express, Mongoose & Socket IO</p>
+            </article>
+            <article className="project">
+                <a href="https://planetino-295eb.web.app/" target="_blank">Planetino</a>
+                <p>React & Framer Motion</p>
+            </article>
+        </section>
+    )
+}
+
+export default Projects
