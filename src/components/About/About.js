e8e45413a554f94b5b49d5372d87bd0125b40450
Georgi <georgichichev15@Gmail.com>
2022-10-06 01:43:53 +0300

    Add about

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return(
+        <section className="aboutMe">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus dolor earum explicabo hic magni
+                optio provident vel. Excepturi, laborum!
+            </p>
+        </section>
+    )
+}
+
+export default About
