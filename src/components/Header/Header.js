d63e4a7af155c813c6e9fd433945d1fad87d65c8
Georgi <georgichichev15@Gmail.com>
2022-10-06 01:43:46 +0300

    Add Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+const Header = () => {
+    return(
+        <header className="header">
+            <h1>Georgi Chichev</h1>
+            <p>Web Developer</p>
+            <nav>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        Projects
+                    </li>
+                    <li>
+                        Info
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
